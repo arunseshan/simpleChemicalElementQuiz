d233172339db986d37b1c580f272b53db3a1c889
arunseshan <seshdroid@gmail.com>
2017-07-07 07:28:09 +0530

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Arun Seshan on 07/07/17.
+//  Copyright © 2017 Arun Seshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
