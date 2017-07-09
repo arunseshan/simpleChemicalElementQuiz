c2356e72afe6f24a51cdeb0958f0084e75aaadb0
arunseshan <seshdroid@gmail.com>
2017-07-09 19:28:36 +0530

    Simple Application that displays various chemical elements and certain properties of it.

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList = ["Carbon","Gold","Chlorine","Sodium"]
+    
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateElement(){
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    
+    @IBAction func goToNextElement(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count{
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
 
 }
 
